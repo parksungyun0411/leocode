fee4aa2f1f3384a98cf5997fdf1076cf0330a7c7
parksungyun0411 <154411170+parksungyun0411@users.noreply.github.com>
2023-12-27 19:45:32 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n=0;
+    int sum=0;
+    scanf("%d", &n);
+    for(int i=1;i<n+1;i++){
+        sum = sum + i;
+        
+    }
+    printf("%d", sum);
+    
+}
