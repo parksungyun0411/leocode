34ccb3c669b206054ed0fbe46e414ba7f3adab4d
parksungyun0411 <154411170+parksungyun0411@users.noreply.github.com>
2023-12-27 21:31:39 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    scanf("%d",&x);
+
+    int a,b=0;
+
+    for(int i=0;i<x;i++){
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",i+1,a,b,a+b);
+    }
+    
+}
