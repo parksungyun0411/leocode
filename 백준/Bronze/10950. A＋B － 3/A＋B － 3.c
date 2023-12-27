ac07233db51bd3d09c48718be985b4893993bf13
parksungyun0411 <154411170+parksungyun0411@users.noreply.github.com>
2023-12-27 21:30:52 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int k;
+    scanf("%d", &k);
+    for(int i=0;i<k;i++){
+        int a,b;
+        scanf("%d %d", &a,&b);
+        printf("%d\n",a+b);
+    }
+    
+}
